1e967d79fc8b1033d3a0c1b20443d1970f51150f
hmdev <=>
2024-04-20 03:29:20 -0300

    segunda parte ok

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,36 +1,58 @@
+import bcrypt from 'bcryptjs';
 import { ObjectId } from "mongodb";
 import { db } from "../data/mongodb";
 import { Response } from "express";
 
 export interface user{
-    _id:string,
-    nome:string,
+    _id:ObjectId,
+    name:string,
     email:string,
+    hash:string,
 }
 
 export class User{
-    constructor(private _id:string, private _nome:string, private _email:string, private _pass:string){}
+    constructor(private _id:ObjectId, private _name:string, private _email:string, private _hash:string){}
     
-    get info():user{
+    get info():{_id:ObjectId, name:string, email:string}{
         return{
             _id:this._id,
-            nome:this._nome,
+            name:this._name,
             email:this._email
         }
     }
 
     async register(response:Response):Promise<void>{
         try{
-            const newObjId = new ObjectId(this._id.replace(/-/g, '').substring(0, 24));
-            await db.collection('users').insertOne({
-                _id:newObjId,
-                name:this._nome,
+            //const newObjId = new ObjectId(this._id.replace(/-/g, '').substring(0, 24));
+            await db.collection<user>('clients').insertOne({
+                _id:this._id,
+                name:this._name,
                 email:this._email,
-                password:this._pass
+                hash:this._hash
             })
-            response.send(`Usuário registrado ${newObjId}`);
+            response.send(`Usuário registrado ${this._id}`);
         }catch(err){
             response.send(err);
+        }
+    }
+
+    async alterarEmail(email:string, pass:string, res:Response){
+        let id = new ObjectId(this._id);
+        try{
+            let result = bcrypt.compareSync(pass, this._hash);
+            if(result){
+                this._email = email;
+                await db.collection<user>('clients').updateOne({_id:id}, {
+                    $set:{
+                        email:email
+                    }
+                })
+                res.status(200).send('Email updated!')
+            }else{
+                res.status(500).send('SENHA INCORRETA, ALTERAÇÃO NÃO AUTORIZADA!')
+            }
+        }catch(err){
+            res.send(err);
         }
     }
 }
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { BSON } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import { db } from '../data/mongodb';
 import multer from 'multer';
 import {v4 as uuidv4 } from 'uuid';
-import { User } from '../models/user';
+import bcrypt from 'bcryptjs';
+import { User, user } from '../models/user';
 
 const upload = multer();
 
@@ -12,23 +13,35 @@ export const routerUsers = express.Router();
 routerUsers.route('/users').get(async (req, res)=>{
     // all users
     try{
-        const data = await db.collection('users').find({}, {limit:4}).toArray();
+        const data = await db.collection('clients').find({}, {limit:4}).toArray();
         res.send(data);
     }catch(err){
         res.send(err);
     }
 })
 
-routerUsers.route('/user/:id').get(async (req, res)=>{
-    // find one user br id
-    try{
-        const id = new BSON.ObjectId(req.params.id);
-        const data = await db.collection('users').findOne({_id:id});
-        res.send(data);
-    }catch(err){
-        res.send(err);
-    }
-})
+routerUsers.route('/user/:id')
+    .get(async (req, res)=>{
+        // find one user br id
+        try{
+            const id = new ObjectId(req.params.id);
+            const data = await db.collection<user>('clients').findOne({_id:id});
+            const user = new User(data?._id as ObjectId, data?.name as string, data?.email as string, data?.hash as string)
+            res.json(user.info);
+        }catch(err){
+            res.send(err);
+        }
+    })
+    .put([express.json() ,async (req:express.Request, res:express.Response)=>{
+        try{
+            let id = new ObjectId(req.params.id);
+            let data = await db.collection<user>('clients').findOne({_id:id});
+            let user = new User(data?._id as ObjectId, data?.name as string, data?.email as string, data?.hash as string)
+            await user.alterarEmail(req.body.email, req.body.pass, res);
+        }catch(err){
+            res.send(err);
+        }
+    }])
 
 routerUsers.route('/add-user').post(upload.none()) // Multipart Form parse
 routerUsers.route('/add-user').post(async (req, res)=>{
@@ -36,7 +49,9 @@ routerUsers.route('/add-user').post(async (req, res)=>{
     try{
         const userForm:{nome:string, email:string, pass:string} = req.body;
         const id = uuidv4();
-        const newUser = new User(id, userForm.nome, userForm.email, userForm.pass);
+        const objId = new ObjectId(id.replace(/-/g, '').substring(0, 24));
+        const hash = bcrypt.hashSync(userForm.pass, 8);
+        const newUser = new User(objId, userForm.nome, userForm.email, hash);
         await newUser.register(res);
     }catch(err){
         res.send(err);
