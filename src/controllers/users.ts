72f5d030951cf1af4be5e629c755169931ac1ee0
hmdev <=>
2024-04-20 20:47:46 -0300

    finish

---
+++ src/controllers/users.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { db } from "../data/mongodb";
+import {ObjectId} from 'mongodb';
+import {v4 as uuidv4} from 'uuid';
+import bcrypt from 'bcryptjs';
+import { user, User } from "../models/user";
+
+//ACTIONS
+
+export async function getAllUsers(req:Request, res:Response){
+    try{
+        let data = await db.collection('clients').find({}).project({
+            _id:1,
+            name:1,
+            email:1,
+        }).toArray();
+        res.json(data);
+    }catch(err){
+        res.send(err);
+    }
+}
+
+export async function getUser(req:Request, res:Response){
+    try{
+        let id = new ObjectId(req.params.id);
+        let data = await db.collection<user>('clients').findOne({_id:id});
+        res.json({
+            id:data?._id,
+            name:data?.name,
+            email:data?.email,
+        });
+    }catch(err){
+        res.send(err);
+    }
+}
+
+export async function addUser(req:Request, res:Response){
+    try{
+        let id = new ObjectId(uuidv4().replace(/-/g, '').substring(0, 24));
+        let hash = bcrypt.hashSync(req.body.pass);
+        let user = new User(id, req.body.name as string, req.body.email as string, hash);
+        await user.register(res);
+    }catch(err){
+        res.send(err);
+    }
+}
+
+export async function putEmail(req:Request, res:Response){
+    try{
+        let id = new ObjectId(req.params.id);
+        let data = await db.collection<user>('clients').findOne({_id:id});
+        let user = new User(id, data?.name as string, data?.email as string, data?.hash as string);
+        await user.alterarEmail(req.body.email, req.body.pass, res);
+    }catch(err){
+        res.send(err);
+    }
+}
+
+export async function delUser(req:Request, res:Response){
+    try{
+        let id = new ObjectId(req.params.id);
+        await db.collection('clients').deleteOne({_id:id});
+        res.send('Usuário removido!')
+    }catch(err){
+        res.send(err);
+    }
+}
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,60 +1,22 @@
 import express from 'express';
-import { ObjectId } from 'mongodb';
-import { db } from '../data/mongodb';
 import multer from 'multer';
-import {v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcryptjs';
-import { User, user } from '../models/user';
+import { getAllUsers, getUser, addUser, putEmail, delUser } from '../controllers/users';
 
 const upload = multer();
 
 export const routerUsers = express.Router();
 
-routerUsers.route('/users').get(async (req, res)=>{
-    // all users
-    try{
-        const data = await db.collection('clients').find({}, {limit:4}).toArray();
-        res.send(data);
-    }catch(err){
-        res.send(err);
-    }
-})
+routerUsers.route('/users').get(getAllUsers);
 
 routerUsers.route('/user/:id')
-    .get(async (req, res)=>{
-        // find one user br id
-        try{
-            const id = new ObjectId(req.params.id);
-            const data = await db.collection<user>('clients').findOne({_id:id});
-            const user = new User(data?._id as ObjectId, data?.name as string, data?.email as string, data?.hash as string)
-            res.json(user.info);
-        }catch(err){
-            res.send(err);
-        }
-    })
-    .put([express.json() ,async (req:express.Request, res:express.Response)=>{
-        try{
-            let id = new ObjectId(req.params.id);
-            let data = await db.collection<user>('clients').findOne({_id:id});
-            let user = new User(data?._id as ObjectId, data?.name as string, data?.email as string, data?.hash as string)
-            await user.alterarEmail(req.body.email, req.body.pass, res);
-        }catch(err){
-            res.send(err);
-        }
-    }])
+    .get(getUser)
+    .delete(delUser)
+
+routerUsers.route('/user/update-email/:id').put([express.json(), putEmail])
 
 routerUsers.route('/add-user').post(upload.none()) // Multipart Form parse
-routerUsers.route('/add-user').post(async (req, res)=>{
-    // add user
-    try{
-        const userForm:{nome:string, email:string, pass:string} = req.body;
-        const id = uuidv4();
-        const objId = new ObjectId(id.replace(/-/g, '').substring(0, 24));
-        const hash = bcrypt.hashSync(userForm.pass, 8);
-        const newUser = new User(objId, userForm.nome, userForm.email, hash);
-        await newUser.register(res);
-    }catch(err){
-        res.send(err);
-    }
+routerUsers.route('/add-user').post(addUser)
 
+routerUsers.use((req, res)=>{
+    res.send(`/collection-user -> ${req.url} - rota não encontrada! `)
 })
