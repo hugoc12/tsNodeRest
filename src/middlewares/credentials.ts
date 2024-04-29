6b6a37a529e1822b73a0727aa38cbe80d1b944de
hmdev <=>
2024-04-28 23:57:43 -0300

    ...

--- src/middlewares/credentials.ts
+++ src/middlewares/credentials.ts
@@ -5,7 +5,6 @@ export function credentials(req:Request, res:Response, next:NextFunction){
     const origin = req.headers.origin as string;
     if(origins_authorized.includes(origin) || !origin){
         res.header('Access-Control-Allow-Origin', '*'); // Liberando acesso a dados retornados pela API
-        console.log('access control true');
     }
     next();
 }
